ae5810dda6802d9131b485735754126322c262b7
Gema Rubio <gemarubio89@gmail.com>
2024-10-14 17:35:58 +0200

    añado ver datos de usuario y ver estadisticas

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'partida.php';
 require_once 'controladorPartida.php';
+require_once 'controladorUsuario.php';
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $paths = $_SERVER['REQUEST_URI'];
 $parametros = explode("/",$paths);
@@ -16,4 +17,13 @@ if($requestMethod=='POST' && strtoupper($parametros[1])=='GAMER' && empty($param
 }else if($requestMethod=='POST' && strtoupper($parametros[1])=='GAMER' && strtoupper($parametros[2])=='CUSTOM'){
    // debe tener correo, longitud del tablero, cantidad de tropas y tantas posiciones como tropas restantes haya :(cantTropas-longitud)/2
     ControladorPartida::iniciarJuegoPersonalizado($body);
+}else if($requestMethod=='POST' && strtoupper($parametros[1])=='USER'&& empty($parametros[2])){
+    ControladorUsuario::consultarDatosUser($body->correo);
+}else if($requestMethod=='POST' && strtoupper($parametros[1])=='USER'&& strtoupper($parametros[2])== 'ESTADISTICA'){
+    ControladorUsuario::verEstadisticas($body->correo);
 }
+/*else if($requestMethod=='GET' && strtoupper($parametros[1]=='USER') && strtoupper($parametros[2])){
+    $v=split($parametros[2],':');
+    $correo=$v[0].'@'.$v[1];
+    ControladorUsuario::consultarDatosUser($correo);
+}*/
